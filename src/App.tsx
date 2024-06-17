ddf212f78227429743da7233159eb21cfe5c8703
kiethohoai <kietho.060193@gmail.com>
2024-06-17 09:08:08 +0700

    31 Get List Users with React

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { increment, decrement } from "./redux/counter/counterSlice";
-import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import Header from "./main/Header";
+import TabsContent from "./main/TabsContent";
+import "./App.css";
 
 function App() {
-  const count = useAppSelector((state) => state.counter.value);
-  const dispatch = useAppDispatch();
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app-container">
+        <Header />
       </div>
-
-      <h2>My Current Count = {count}</h2>
-      <div style={{ marginTop: "8px" }}>
-        <button className="btn btn-outline-success" onClick={() => dispatch(decrement())}>
-          Increase -1
-        </button>
-        <button className="btn btn-outline-danger" onClick={() => dispatch(increment())}>
-          Increase +1
-        </button>
+      <div className="app-content">
+        <TabsContent />
       </div>
     </>
   );
 }
 
 export default App;
+3;
---
+++ src/main/TabsContent.tsx
@@ -0,0 +1,18 @@
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import UsersTable from "./UsersTable";
+
+function TabsContent() {
+  return (
+    <Tabs defaultActiveKey="users" id="uncontrolled-tab-example" className="mb-3 mt-3">
+      <Tab eventKey="users" title="Users">
+        <UsersTable />
+      </Tab>
+      <Tab eventKey="blogs" title="Blogs">
+        Tab content for Profile
+      </Tab>
+    </Tabs>
+  );
+}
+
+export default TabsContent;
