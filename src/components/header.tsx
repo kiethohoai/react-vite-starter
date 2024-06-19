4b9c204fa02b5ba3b62039aee1208e31d51fbb3e
kiethohoai <kietho.060193@gmail.com>
2024-06-19 08:14:04 +0700

    39 Light/Dark Mode with Bootstrap

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,31 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { useAppSelector } from "../hooks";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 function Header() {
   const users = useAppSelector((state) => state.user.listUsers);
+  const [mode, setMode] = useState(false);
 
   return (
-    <Navbar className="bg-body-tertiary">
+    <Navbar
+      className="bg-body-tertiary"
+      data-bs-theme={mode === false ? "light" : "dark"}
+    >
       <Container>
-        <Navbar.Brand href="#home">React Redux = {users.length}</Navbar.Brand>
+        <Navbar.Brand href="#home">React Redux</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
+            <Form>
+              <Form.Check
+                type="switch"
+                id="custom-switch"
+                label={mode === true ? "Dark Mode" : "Light Mode"}
+                onChange={(e) => setMode(e.target.checked)}
+              />
+            </Form>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
